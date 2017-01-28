ac4599498cf0042741235561eece8691f87ea0be
Sam <sjy@exilesoft.local>
2017-01-28 20:14:12 +0530

    simple functions

---
+++ src/koans/SimpleFunctions.kt
@@ -0,0 +1,18 @@
+package koans
+
+/**
+ * Created by Sam on 1/28/2017.
+ */
+/*
+
+http://try.kotlinlang.org/#/Kotlin%20Koans/Introduction/Hello,%20world!/Task.kt
+
+Simple Functions
+Take a look at function syntax and make the function start return the string "OK".
+In the tasks the function TODO() is used that throws an exception.
+Your job during the koans will be to replace this function invocation with a meaningful code according to the problem.
+
+fun start(): String = TODO()
+
+ */
+fun start(): String = "OK"
